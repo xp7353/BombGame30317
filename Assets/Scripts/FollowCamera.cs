3a97d073d863040aa21d60bc97fac0114b9813c4
xp7353 <80015324+xp7353@users.noreply.github.com>
2021-05-11 12:11:34 +0900

    5/11 배경 플레이어 생성

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target; public float dist = 5.0f; public float height = 4.0f; public float smoothRotate = 5.0f;
+
+    void LateUpdate()
+    {
+        float currYAngle = Mathf.LerpAngle(transform.eulerAngles.y, target.eulerAngles.y, smoothRotate * Time.deltaTime);
+
+        Quaternion rot = Quaternion.Euler(0, currYAngle, 0); 
+        transform.position = target.position - (rot * Vector3.forward * dist) + (Vector3.up * height); 
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float translation; private float rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        translation = Input.GetAxis("Vertical") * 10f;
+        rotation = Input.GetAxis("Horizontal") * 100f;
+
+        transform.Translate(0, 0, translation * Time.deltaTime);
+        transform.Rotate(0, rotation * Time.deltaTime, 0);
+    }
+}
